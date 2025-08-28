c60b977d041cdcc0542e2e1f0694b42c3c7e986a
Geodor Ruales <gandor999@gmail.com>
2025-08-29 00:09:06 +0800

    refactor some unneeded things

--- app/src/main/java/com/gandor/mobile_locator/layers/ui/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/gandor/mobile_locator/layers/ui/viewmodels/BaseViewModel.kt
@@ -2,6 +2,7 @@ package com.gandor.mobile_locator.layers.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.gandor.mobile_locator.layers.data.constants.exceptions.ComposableException
+import com.gandor.mobile_locator.layers.ui.viewmodels.interfaces.ErrorThrower
 import com.gandor.mobile_locator.layers.ui.viewmodels.states.BaseState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
--- app/src/main/java/com/gandor/mobile_locator/layers/ui/viewmodels/interfaces/ErrorThrower.kt
+++ app/src/main/java/com/gandor/mobile_locator/layers/ui/viewmodels/interfaces/ErrorThrower.kt
@@ -1,4 +1,4 @@
-package com.gandor.mobile_locator.layers.ui.viewmodels
+package com.gandor.mobile_locator.layers.ui.viewmodels.interfaces
 
 import com.gandor.mobile_locator.layers.data.constants.exceptions.ComposableException
 
