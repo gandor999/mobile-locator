81e34f32a0879d0fe754277e8ff90d3d875442a3
Geodor Ruales <gandor999@gmail.com>
2025-08-28 21:01:43 +0800

    add localhost to baseurl

--- app/src/main/java/com/gandor/mobile_locator/layers/data/constants/RemoteHosts.kt
+++ app/src/main/java/com/gandor/mobile_locator/layers/data/constants/RemoteHosts.kt
@@ -1,5 +1,8 @@
 package com.gandor.mobile_locator.layers.data.constants
 
+// Do this command when testing on a physical phone: adb reverse tcp:8000 tcp:8000
+// Use 10.0.2.2:8080 if testing on emulator
 object RemoteHosts {
-    const val MOBILE_LOCATOR_HOST: String = "http://10.0.2.2:8080/locations/"
+//    const val MOBILE_LOCATOR_HOST: String = "http://10.0.2.2:8080/locations/"
+    const val MOBILE_LOCATOR_HOST: String = "http://localhost:8080/locations/"
 }
